9e75d168f60e12bc5945e9fc1e5058a13f3cdde9
Mohannadz <mohannad@mwadi.com>
2015-08-30 04:15:56 +0300

    Updates

--- page.tpl.php
+++ page.tpl.php
@@ -1,15 +1,7 @@
 <div id="page-wrapper">
   <div id="top-bar">
     <div class="wrapper">
-      <div class="collect left"><?php print t("Are getting married?");?> <a href="#"><?php print t("Start Here");?></a></div>
-      
-      <div id="top-icons" class="right"> <a id="follow-twitter" class="follow-icon" target="_blank" href="http://twitter.com/arabiaweddings"> Follow us on Twitter</a> <a id="follow-facebook" class="follow-icon" target="_blank" href="http://www.facebook.com/ArabiaWeddings"> Our Facebook page</a> <a id="follow-pinterest" class="follow-icon" target="_blank" href="http://pinterest.com/arabiaweddings"> Pinterest</a> <a id="follow-instagram" class="follow-icon" target="_blank" href="http://instagram.com/arabiaweddings">instagram</a> <a id="follow-rss" class="follow-icon" target="_blank" href="/rss"> RSS Feed</a> <?php print render($page['search']);?> </div>
-      
-      <div id="lang" class="right"><?php print render($page['language_switcher']);?> </div>
-    </div>
-  </div>
-  <div id="page" class="wrapper">
-    <div id="header">
+    	<div class="collect left"><?php print t("Are you getting married?");?> <a href="#"><?php print t("Start Here");?></a></div>
       <div class="user-call-links right">
         <?php
       	global $user;
@@ -20,6 +12,19 @@
         <div class="home-action-butons"> <a class="call-to-action" href="<?php print url('user/register'); ?>"><?php print t('Register'); ?></a> <a class="call-to-action" href="<?php print url('user/login'); ?>"><?php print t('Login'); ?></a> </div>
         <?php } ?>
       </div>
+    </div>
+  </div>
+  <div id="page" class="wrapper">
+    <div id="header">
+          <div id="top-icons" class="right">
+      	<a id="follow-twitter" class="follow-icon hidden" target="_blank" href="http://twitter.com/arabiaweddings"> Follow us on Twitter</a>
+        <a id="follow-facebook" class="follow-icon hidden" target="_blank" href="http://www.facebook.com/ArabiaWeddings"> Our Facebook page</a>
+        <a id="follow-pinterest" class="follow-icon hidden" target="_blank" href="http://pinterest.com/arabiaweddings"> Pinterest</a>
+        <a id="follow-instagram" class="follow-icon hidden" target="_blank" href="http://instagram.com/arabiaweddings">instagram</a>
+        <a id="follow-rss" class="follow-icon hidden" target="_blank" href="/rss"> RSS Feed</a> <?php print render($page['search']);?> </div>
+      <div id="lang" class="right"><?php print render($page['language_switcher']);?> </div>
+
+      
       <div class="clear"></div>
       <div id="logo"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"> <img src="<?php print $logo ?>" alt="<?php print $site_name;?>" title="<?php print $site_name;?>" /></a></div>
       <div id="main_menu">
@@ -30,6 +35,8 @@
         <div class="clear"></div>
       </div>
     </div>
+    
+    <?php if(arg(1) != 364381) {?>
     <div id="nav-bar">
       <?php
 				$s=array('class="menu"');
@@ -37,10 +44,12 @@
 				print str_replace($s,$r,render($page['nav_menu']));?>
     </div>
     <div class="clear"></div>
-    <div class="leader-board"> <?php print render($page['leaderboard']);?> </div>
-    <div class="clear"></div>
-    <?php if ($breadcrumb): ?>
-    <div id="breadcrumb" class="hidden"><?php print $breadcrumb; ?></div>
+    <?php }?>
+    
+    <?php if(arg(1) != 364381) {?><div class="leader-board"> <?php print render($page['leaderboard']);?> </div>
+    <div class="clear"></div> <?php } ?>
+    <?php if ($breadcrumb && arg(1) != 364381): ?>
+    <div id="breadcrumb"><?php print $breadcrumb; ?></div>
     <?php endif; ?>
     <?php print $messages; ?>
     <div id="main-wrapper">
@@ -51,7 +60,7 @@
             <div id="highlighted"><?php print render($page['highlighted']); ?></div>
             <?php endif; ?>
             <a id="main-content"></a> <?php print render($title_prefix); ?>
-            <?php if ($title): ?>
+            <?php if ($title  && arg(1) != 364381): ?>
             <h1 class="title" id="page-title"><?php print $title; ?></h1>
             <?php endif; ?>
             <?php print render($title_suffix); ?>
@@ -83,12 +92,18 @@
       </div>
     </div>
     <!-- /#main, /#main-wrapper -->
-    
-    <div id="footer">
-      <div class="section"> <?php print render($page['footer']); ?> </div>
-    </div>
-    <!-- /.section, /#footer --> 
-    
   </div>
 </div>
 <!-- /#page, /#page-wrapper -->
+    
+<footer>
+    <div class="wrapper">
+      <div class="footer_item"> <?php print render($page['footer_menu_1']);?> </div>
+      <!--footer_item-->
+      <div class="footer_item"> <?php print render($page['footer_menu_2']);?> </div>
+      <!--footer_item-->
+      <div class="footer_item"> <?php print render($page['footer_menu_3']);?> </div>
+      <!--footer_item-->
+      <div class="footer_item last"> <?php print render($page['footer_menu_4']);?>   <?php print render($page['newsletter_footer']);?> </div></div>
+</footer>
+<div class="footer-message"><span><?php print t("All Rights Reserved ") .date('Y'); ?></span></div>
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 jQuery(document).ready(function(e) {
-			jQuery("#new-gallery").gridalicious({
-    gutter: 15,
+	jQuery("#new-gallery").gridalicious({
+		gutter: 15,
     width: 315,
+    animate: true,
+    	animationOptions: {
+        speed: 150,
+        duration: 300,
+   	 },
+		});
+		
+		jQuery(".grid-items").gridalicious({
+		gutter: 20,
+    width: 430,
     animate: true,
     	animationOptions: {
         speed: 150,
@@ -10,4 +20,3 @@ jQuery(document).ready(function(e) {
 		});
 
 });
-
